cbc8693028c3305f4d6261867de4980604621f5f
PGautam27 <20010115510.pgautam@cse.reva.edu.in>
2022-04-27 16:17:45 +0530

    SDP

---
+++ app/src/main/java/com/example/publicproblemtrackingapp/view/screens/Screen.kt
@@ -0,0 +1,12 @@
+package com.example.publicproblemtrackingapp.view.screens
+
+sealed class Screen(route:String){
+    object HomeScreen : Screen("home_screen")
+    object UserScreen : Screen("user_screen")
+    object AdminScreen : Screen("admin_screen")
+    object UserRegisterScreen : Screen("user_register_screen")
+    object AdminRegisterScreen : Screen("admin_register_screen")
+    object UserLoginScreen : Screen("user_login_screen")
+    object AdminLoginScreen : Screen("admin_login_screen")
+    object MapScreen : Screen("map_screen")
+}
