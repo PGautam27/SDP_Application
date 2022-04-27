ff6ce3a16c4f4370b106b8efa8d5ea3e63bdd5e7
PGautam27 <20010115510.pgautam@cse.reva.edu.in>
2022-04-27 15:29:26 +0530

    SDP

--- app/src/main/java/com/example/publicproblemtrackingapp/view/MainActivity.kt
+++ app/src/main/java/com/example/publicproblemtrackingapp/view/MainActivity.kt
@@ -3,11 +3,14 @@ package com.example.publicproblemtrackingapp.view
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.publicproblemtrackingapp.ui.theme.PublicProblemTrackingAppTheme
@@ -17,12 +20,12 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             PublicProblemTrackingAppTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
+                Column(
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    modifier = Modifier.fillMaxSize()
                 ) {
-                    Greeting("Android")
+
                 }
             }
         }
