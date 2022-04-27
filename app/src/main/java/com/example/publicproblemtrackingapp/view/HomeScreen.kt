82d485faf2a4004b3f89342ea62753e7e40faaa1
PGautam27 <20010115510.pgautam@cse.reva.edu.in>
2022-04-27 17:19:42 +0530

    SDP

--- app/src/main/java/com/example/publicproblemtrackingapp/view/HomeScreen.kt
+++ app/src/main/java/com/example/publicproblemtrackingapp/view/HomeScreen.kt
@@ -8,7 +8,6 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.TextStyle
