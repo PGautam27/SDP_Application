1062631985e7341a0597c4bccab5d984b96fdcf8
PGautam27 <20010115510.pgautam@cse.reva.edu.in>
2022-04-27 15:26:01 +0530

    SDP

---
+++ app/src/main/java/com/example/publicproblemtrackingapp/PublicProblemTrackingApp.kt
@@ -0,0 +1,7 @@
+package com.example.publicproblemtrackingapp
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class PublicProblemTrackingApp : Application()
