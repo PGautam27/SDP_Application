f3c5936699abd04e259d38b7ef6d062826004659
PGautam27 <20010115510.pgautam@cse.reva.edu.in>
2022-04-27 17:16:22 +0530

    SDP

--- app/src/main/java/com/example/publicproblemtrackingapp/view/user/UserScreen.kt
+++ app/src/main/java/com/example/publicproblemtrackingapp/view/user/UserScreen.kt
@@ -9,8 +9,12 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.publicproblemtrackingapp.ui.theme.Orange
 import com.example.publicproblemtrackingapp.ui.theme.Yellow
 
@@ -23,8 +27,8 @@ fun UserScreen() {
             .background(color = Yellow)
             .fillMaxSize()
     ) {
-        Text(text = "Already Have an Account")
-        Spacer(modifier = Modifier.padding(20.dp))
+        Text(text = "Already Have an Account", style = TextStyle(color = Orange, fontSize = 25.sp,fontWeight = FontWeight.Bold))
+        Spacer(modifier = Modifier.padding(10.dp))
         Button(
             onClick = { /*TODO*/ },
             colors = ButtonDefaults.buttonColors(
@@ -32,18 +36,18 @@ fun UserScreen() {
                 contentColor = Yellow
             ),
             modifier = Modifier
-                .height(LocalConfiguration.current.screenHeightDp.dp / 8)
+                .height(LocalConfiguration.current.screenHeightDp.dp / 10)
                 .width(
-                    LocalConfiguration.current.screenWidthDp.dp - 10.dp
+                    LocalConfiguration.current.screenWidthDp.dp - 40.dp
                 )
         ) {
             Text(text = "LOGIN")
         }
         Spacer(modifier = Modifier.padding(10.dp))
-        Divider(color = Orange, thickness = 2.dp)
+        Divider(color = Orange, thickness = 2.dp, modifier = Modifier.width(LocalConfiguration.current.screenWidthDp.dp -80.dp))
         Spacer(modifier = Modifier.padding(10.dp))
-        Text(text = "Don't have One?")
-        Spacer(modifier = Modifier.padding(20.dp))
+        Text(text = "Don't have One?",style = TextStyle(color = Orange, fontSize = 25.sp, fontWeight = FontWeight.Bold))
+        Spacer(modifier = Modifier.padding(10.dp))
         Button(
             onClick = { /*TODO*/ },
             colors = ButtonDefaults.buttonColors(
@@ -51,9 +55,9 @@ fun UserScreen() {
                 contentColor = Yellow
             ),
             modifier = Modifier
-                .height(LocalConfiguration.current.screenHeightDp.dp / 8)
+                .height(LocalConfiguration.current.screenHeightDp.dp / 10)
                 .width(
-                    LocalConfiguration.current.screenWidthDp.dp - 10.dp
+                    LocalConfiguration.current.screenWidthDp.dp - 40.dp
                 )
         ) {
             Text(text = "SIGN UP")
