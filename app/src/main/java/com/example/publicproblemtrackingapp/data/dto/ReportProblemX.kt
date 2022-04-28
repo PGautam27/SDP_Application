46e970fcc663ae7f9b78973d39c5d00a6a9fb989
PGautam27 <20010115510.pgautam@cse.reva.edu.in>
2022-04-28 17:49:02 +0530

    SDP

---
+++ app/src/main/java/com/example/publicproblemtrackingapp/data/dto/ReportProblemX.kt
@@ -0,0 +1,8 @@
+package com.example.publicproblemtrackingapp.data.dto
+
+data class ReportProblem(
+    val Category: String,
+    val Date: String,
+    val Location: String,
+    val Reason: String
+)
